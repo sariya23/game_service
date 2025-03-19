5902f492fe9d18e261634929cdb38e49f3e2d48a
sariya <dayzwaitik123@yandex.ru>
2025-03-19 13:29:31 +0300

    Описал интерфейс GameServicer

--- internal/grpchandlers/grpchandlers.go
+++ internal/grpchandlers/grpchandlers.go
@@ -1,11 +1,23 @@
 package grpchandlers
 
 import (
+	"context"
+
+	"github.com/sariya23/game_service/internal/model"
 	gamev4 "github.com/sariya23/proto_api_games/v4/gen/game"
 	"google.golang.org/grpc"
 )
 
+type GameFilters struct {
+	ReleaseYear string
+	Genre       string
+	Tags        []string
+}
+
 type GameServicer interface {
+	AddGame(ctx context.Context, game model.Game) (createdGame model.Game, err error)
+	GetGame(ctx context.Context, gameTitle string) (game model.Game, err error)
+	GetTopGames(ctx context.Context, gameFilters GameFilters, limit int32) (games []model.Game, err error)
 }
 
 type serverAPI struct {
