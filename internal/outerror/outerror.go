df21e5da946ba9eaca509ee4e2e1fcf35f8f408f
sariya <dayzwaitik123@yandex.ru>
2025-03-19 20:42:38 +0300

    Вынес сообщения ошибки запроса в переменные

--- internal/outerror/outerror.go
+++ internal/outerror/outerror.go
@@ -5,3 +5,9 @@ import "errors"
 var (
 	ErrGameAlreadyExist = errors.New("game with this title already exist")
 )
+
+var (
+	TitleRequiredMessage       = "Title is required field"
+	DescriptionRequiredMessage = "Description is required field"
+	ReleaseYearRequiredMessage = "Release Year is required field"
+)
