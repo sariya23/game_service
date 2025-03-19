76d824e842b082e8cb45f067de52e42803730a8d
sariya <dayzwaitik123@yandex.ru>
2025-03-19 13:19:05 +0300

    Добавил регистрацию grpc хендлеров

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/sariya23/game_service/internal/config"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+	log := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+	log.Info(
+		"starting game service",
+		slog.Int("grpc port", cfg.GrpcServerPort),
+		slog.Int("htpp port", cfg.HttpServerPort),
+	)
 }
