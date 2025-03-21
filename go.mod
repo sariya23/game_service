a6a11b35f95dc246a8ae5507650d9070662c31c2
sariya <dayzwaitik123@yandex.ru>
2025-03-21 14:41:43 +0300

    Создал пакет для отправки email

--- go.mod
+++ go.mod
@@ -42,6 +42,8 @@ require (
 	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- internal/lib/email/email.go
+++ internal/lib/email/email.go
@@ -1,1 +1,27 @@
 package email
+
+import "gopkg.in/gomail.v2"
+
+type EmailDialer struct {
+	dialer gomail.Dialer
+}
+
+func NewDialer(smtpHost string, smtpPort int, user string, Password string) *EmailDialer {
+	dialer := gomail.NewDialer(smtpHost, smtpPort, user, Password)
+	return &EmailDialer{
+		dialer: *dialer,
+	}
+}
+
+func (dialer *EmailDialer) SendMessage(from string, to string, subject string, body string) error {
+	message := gomail.NewMessage()
+	message.SetHeader("From", from)
+	message.SetHeader("To", to)
+	message.SetHeader("Subject", subject)
+	message.SetBody("text/plain", body)
+
+	if err := dialer.dialer.DialAndSend(message); err != nil {
+		return err
+	}
+	return nil
+}
