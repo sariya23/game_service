648094f3d4a58347602f43e5905c1c504a2881b3
sariya <dayzwaitik123@yandex.ru>
2025-03-21 17:53:44 +0300

    Перенес тесты email в интеграционные

--- tests/email_test.go
+++ tests/email_test.go
@@ -1,15 +1,19 @@
-package email
+//go:build integration
+// +build integration
+
+package tests
 
 import (
 	"testing"
 
 	"github.com/sariya23/game_service/internal/config"
+	"github.com/sariya23/game_service/internal/lib/email"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSendMessage(t *testing.T) {
 	cfg := config.MustLoadByPath("../../../config/local.env")
-	sender := NewDialer(cfg.SmtpHost, cfg.SmtpPort, cfg.EmailUser, cfg.EmailPassword)
+	sender := email.NewDialer(cfg.SmtpHost, cfg.SmtpPort, cfg.EmailUser, cfg.EmailPassword)
 	err := sender.SendMessage(cfg.EmailUser, cfg.AdminEmail, "Alert", "Hello")
 	require.NoError(t, err)
 }
