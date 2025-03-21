adbf1d271abf7fb023b4dcaae207361b94029c1e
sariya <dayzwaitik123@yandex.ru>
2025-03-21 15:07:25 +0300

    Тест отправки письма по smtp

--- internal/lib/email/email_test.go
+++ internal/lib/email/email_test.go
@@ -4,9 +4,12 @@ import (
 	"testing"
 
 	"github.com/sariya23/game_service/internal/config"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSendMessage(t *testing.T) {
-	cfg := config.MustLoadByPath("./config/local.env")
-	sender := NewDialer()
+	cfg := config.MustLoadByPath("../../../config/local.env")
+	sender := NewDialer(cfg.SmtpHost, cfg.SmtpPort, cfg.EmailUser, cfg.EmailPassword)
+	err := sender.SendMessage(cfg.EmailUser, cfg.AdminEmail, "Alert", "Hello")
+	require.NoError(t, err)
 }
