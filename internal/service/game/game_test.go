ad7b8c6fb0e874bc5f28204be17eadef7c6658d9
sariya <dayzwaitik123@yandex.ru>
2025-03-23 01:21:24 +0300

    Добавил структуру для SAGA транзакции

--- internal/service/game/game_test.go
+++ internal/service/game/game_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/sariya23/game_service/internal/lib/mockslog"
 	"github.com/sariya23/game_service/internal/outerror"
+	"github.com/sariya23/game_service/internal/storage/postgresql"
 	gamev4 "github.com/sariya23/proto_api_games/v4/gen/game"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -40,6 +41,15 @@ func (m *mockS3Storager) Save(ctx context.Context, data io.Reader, key string) e
 	return args.Error(0)
 }
 
+type mockGameSaver struct {
+	mock.Mock
+}
+
+func (m *mockGameSaver) SaveGame(ctx context.Context, game *gamev4.Game) postgresql.GameTransaction {
+	args := m.Called(ctx, game)
+	return args.Get(0).(postgresql.GameTransaction)
+}
+
 func (m *mockS3Storager) Get(ctx context.Context, bucket, key string) io.Reader {
 	args := m.Called(ctx, bucket, key)
 	return args.Get(0).(io.Reader)
@@ -47,9 +57,10 @@ func (m *mockS3Storager) Get(ctx context.Context, bucket, key string) io.Reader 
 
 func TestAddGame(t *testing.T) {
 	gameProviderMock := new(mockGameProvider)
+	gameSaverMock := new(mockGameSaver)
 	kafkaMock := new(mockKafkaProducer)
 	s3Mock := new(mockS3Storager)
-	gameService := NewGameService(mockslog.NewDiscardLogger(), kafkaMock, gameProviderMock, s3Mock)
+	gameService := NewGameService(mockslog.NewDiscardLogger(), kafkaMock, gameProviderMock, s3Mock, gameSaverMock)
 	t.Run("Нельзя добавить игру, так как она уже есть в БД", func(t *testing.T) {
 		expectedError := outerror.ErrGameAlreadyExist
 		game := gamev4.Game{
