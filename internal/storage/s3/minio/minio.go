95abf2b6ede0740192ebfbc6008b5d5cf8868aff
sariya <dayzwaitik123@yandex.ru>
2025-05-02 16:57:16 +0300

    create minio constructor

--- internal/storage/s3/minio/minio.go
+++ internal/storage/s3/minio/minio.go
@@ -5,15 +5,38 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 type Minio struct {
 	log        *slog.Logger
+	client     *minio.Client
 	BucketName string
 }
 
-func NewMinioClient(log *slog.Logger, bucketName string) *Minio {
-	return &Minio{log: log, BucketName: bucketName}
+func NewMinioClient(
+	log *slog.Logger,
+	host string,
+	port string,
+	bucketName string,
+	accessKey string,
+	secretKey string,
+	useSSL bool,
+) *Minio {
+	client, err := minio.New(fmt.Sprintf("%s:%s", host, port), &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return &Minio{
+		log:        log,
+		client:     client,
+		BucketName: bucketName,
+	}
 }
 
 func (m Minio) SaveObject(ctx context.Context, name string, data io.Reader) (string, error) {
