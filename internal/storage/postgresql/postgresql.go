9cdf1778cca959afc41532d6b114025355876fd5
nikita <dayzwaitik123@yandex.ru>
2025-09-08 09:28:13 +0300

    Добавил метод для получения тэгов

--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -299,7 +299,33 @@ func (postgresql PostgreSQL) DaleteGame(ctx context.Context, gameID uint64) (*mo
 }
 
 func (postgresql PostgreSQL) GetTags(ctx context.Context, tags []string) ([]model.Tag, error) {
-	panic("impl GetTags")
+	const operationPlace = "postgresql.GetTags"
+	log := postgresql.log.With("operationPlace", operationPlace)
+	getTagsQuery := fmt.Sprintf("select %s, %s from tag where %s=$1", tagTagIDFieldName, tagTagNameFieldName, tagTagNameFieldName)
+	tagRows, err := postgresql.connection.Query(ctx, getTagsQuery, tags)
+	if err != nil {
+		log.Error(fmt.Sprintf("Cannot get tags, uncaught error: %v", err))
+		return nil, fmt.Errorf("%s: %w", operationPlace, err)
+	}
+	defer tagRows.Close()
+	tagModels := make([]model.Tag, 0, len(tags))
+	for tagRows.Next() {
+		var modelTag model.Tag
+		err = tagRows.Scan(&modelTag.TagID, &modelTag.TagName)
+		if err != nil {
+			log.Error(fmt.Sprintf("Cannot scan game tags, uncaught error: %v", err))
+			return nil, fmt.Errorf("%s: %w", operationPlace, err)
+		}
+		tagModels = append(tagModels, modelTag)
+	}
+	if tagRows.Err() != nil {
+		log.Error(fmt.Sprintf("Uncaught error: %v", err))
+		return nil, fmt.Errorf("%s: %w", operationPlace, err)
+	}
+	if len(tagModels) != len(tags) {
+		return nil, fmt.Errorf("%s: %w", operationPlace, outerror.ErrTagNotFound)
+	}
+	return tagModels, nil
 }
 
 func (postgressql PostgreSQL) GetGenres(ctx context.Context, genres []string) ([]model.Genre, error) {
