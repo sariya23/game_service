a656bbe3664ebd500d12fdc3b587678e0ff5f5dd
nikita <dayzwaitik123@yandex.ru>
2025-09-11 09:31:49 +0300

    Добавил метод для получения всех жанров

--- internal/storage/postgresql/genre.go
+++ internal/storage/postgresql/genre.go
@@ -37,3 +37,31 @@ func (postgresql PostgreSQL) GetGenreByNames(ctx context.Context, genres []strin
 	}
 	return genreModels, nil
 }
+
+// GetGenres возвращает все жанры.
+func (postgresql PostgreSQL) GetGenres(ctx context.Context) ([]model.Genre, error) {
+	const operationPlace = "postgresql.GetGenres"
+	log := postgresql.log.With("operationPlace", operationPlace)
+	getGenreQuery := fmt.Sprintf("select %s, %s from genre", genreGenreIDFieldName, genreGenreNameFieldName)
+	genreRows, err := postgresql.connection.Query(ctx, getGenreQuery)
+	if err != nil {
+		log.Error(fmt.Sprintf("Cannot get all tags, uncaught error: %v", err))
+		return nil, fmt.Errorf("%s: %w", operationPlace, err)
+	}
+	defer genreRows.Close()
+	var genreModels []model.Genre
+	for genreRows.Next() {
+		var modelGenre model.Genre
+		err = genreRows.Scan(&modelGenre.GenreID, &modelGenre.GenreName)
+		if err != nil {
+			log.Error(fmt.Sprintf("Cannot scan tags, uncaught error: %v", err))
+			return nil, fmt.Errorf("%s: %w", operationPlace, err)
+		}
+		genreModels = append(genreModels, modelGenre)
+	}
+	if genreRows.Err() != nil {
+		log.Error(fmt.Sprintf("Uncaught error: %v", err))
+		return nil, fmt.Errorf("%s: %w", operationPlace, err)
+	}
+	return genreModels, nil
+}
