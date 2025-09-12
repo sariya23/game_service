4a82c9709f62796c48d9a4dbbcf5439c92535d0a
nikita <dayzwaitik123@yandex.ru>
2025-09-12 09:50:34 +0300

    Добавил интеграционные тесты на AddGame

--- tests/game_service_test.go
+++ tests/game_service_test.go
@@ -11,13 +11,16 @@ import (
 	"github.com/sariya23/game_service/internal/lib/mockslog"
 	"github.com/sariya23/game_service/internal/lib/random"
 	"github.com/sariya23/game_service/internal/model"
+	"github.com/sariya23/game_service/internal/outerror"
 	"github.com/sariya23/game_service/internal/storage/postgresql"
 	minioclient "github.com/sariya23/game_service/internal/storage/s3/minio"
 	gamev4 "github.com/sariya23/proto_api_games/v4/gen/game"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 func TestAddGame(t *testing.T) {
@@ -68,5 +71,59 @@ func TestAddGame(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, gameToAdd.CoverImage, imageBytes)
 
+	})
+	t.Run("Игра не создается если передан хотя бы один несущетвующий тэг", func(t *testing.T) {
+		gameToAdd := random.RandomAddGameRequest()
+		tags, err := db.GetTags(ctx)
+		require.NoError(t, err)
+		gameToAdd.Tags = append(model.TagNames(tags), gameToAdd.Tags...)
+		gameToAdd.Genres = nil
+		expectedImage, err := random.Image()
+		require.NoError(t, err)
+		gameToAdd.CoverImage = expectedImage
+		request := gamev4.AddGameRequest{Game: gameToAdd}
+		resp, err := grpcClient.AddGame(ctx, &request)
+		s, _ := status.FromError(err)
+		require.Equal(t, codes.InvalidArgument, s.Code())
+		require.Equal(t, outerror.TagNotFoundMessage, s.Message())
+		require.Zero(t, resp.GetGameId())
+	})
+	t.Run("Игра не создается если передан хотя бы один несущетвующий жанр", func(t *testing.T) {
+		gameToAdd := random.RandomAddGameRequest()
+		genres, err := db.GetGenres(ctx)
+		require.NoError(t, err)
+		gameToAdd.Genres = append(model.GenreNames(genres), gameToAdd.Genres...)
+		gameToAdd.Tags = nil
+		expectedImage, err := random.Image()
+		require.NoError(t, err)
+		gameToAdd.CoverImage = expectedImage
+		request := gamev4.AddGameRequest{Game: gameToAdd}
+		resp, err := grpcClient.AddGame(ctx, &request)
+		s, _ := status.FromError(err)
+		require.Equal(t, codes.InvalidArgument, s.Code())
+		require.Equal(t, outerror.GenreNotFoundMessage, s.Message())
+		require.Zero(t, resp.GetGameId())
+	})
+	t.Run("Нельзя сохранить точно такую же игру", func(t *testing.T) {
+		gameToAdd := random.RandomAddGameRequest()
+		gameToAdd.Genres = nil
+		gameToAdd.Tags = nil
+		expectedImage, err := random.Image()
+		require.NoError(t, err)
+		gameToAdd.CoverImage = expectedImage
+		request := gamev4.AddGameRequest{Game: gameToAdd}
+		resp, err := grpcClient.AddGame(ctx, &request)
+		require.NoError(t, err)
+		require.NotZero(t, resp.GetGameId())
+
+		duplicateGame := random.RandomAddGameRequest()
+		duplicateGame.Title = gameToAdd.GetTitle()
+		duplicateGame.ReleaseDate = gameToAdd.GetReleaseDate()
+		duplicateRequest := gamev4.AddGameRequest{Game: duplicateGame}
+		resp, err = grpcClient.AddGame(ctx, &duplicateRequest)
+		s, _ := status.FromError(err)
+		require.Equal(t, codes.AlreadyExists, s.Code())
+		require.Equal(t, outerror.GameAlreadyExistMessage, s.Message())
+		require.Zero(t, resp.GetGameId())
 	})
 }
