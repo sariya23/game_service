568a273bcac4270f049f76ba5659402caaeca8f2
nikita <dayzwaitik123@yandex.ru>
2025-09-23 12:20:18 +0300

    Во всех тестах использую suite

--- tests/game_service_add_game_test.go
+++ tests/game_service_add_game_test.go
@@ -1,46 +1,24 @@
 package tests
 
 import (
-	"context"
-	"net"
-	"strconv"
 	"testing"
 
-	"github.com/sariya23/game_service/internal/config"
-	"github.com/sariya23/game_service/internal/lib/mockslog"
 	"github.com/sariya23/game_service/internal/lib/random"
 	"github.com/sariya23/game_service/internal/model"
-	"github.com/sariya23/game_service/internal/storage/postgresql"
-	minioclient "github.com/sariya23/game_service/internal/storage/s3/minio"
 	checkers "github.com/sariya23/game_service/tests/checkers"
 	hadlerchecker "github.com/sariya23/game_service/tests/checkers/handlers"
+	"github.com/sariya23/game_service/tests/suite"
 	gamev4 "github.com/sariya23/proto_api_games/v4/gen/game"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func TestAddGame(t *testing.T) {
-	ctx := context.Background()
-	cfg := config.MustLoadByPath("../config/local.env")
-	db := postgresql.MustNewConnection(ctx, mockslog.NewDiscardLogger(), cfg.Postgres.PostgresURL)
-	s3 := minioclient.MustPrepareMinio(ctx, mockslog.NewDiscardLogger(), cfg.Minio, false)
-	conn, err := grpc.NewClient(
-		net.JoinHostPort("127.0.0.1", strconv.Itoa(cfg.Server.GrpcServerPort)),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	if err != nil || conn == nil {
-		t.Fatalf("cannot start client; err = %v", err)
-	}
-	grpcClient := gamev4.NewGameServiceClient(conn)
-	if grpcClient == nil {
-		t.Fatal("cannot create grpcClient")
-	}
+	ctx, suite := suite.NewSuite(t)
 	t.Run("Тест ручки AddGame; Успешное сохранение игры; все поля", func(t *testing.T) {
 		gameToAdd := random.RandomAddGameRequest()
-		tags, err := db.GetTags(ctx)
+		tags, err := suite.Db.GetTags(ctx)
 		require.NoError(t, err)
-		genres, err := db.GetGenres(ctx)
+		genres, err := suite.Db.GetGenres(ctx)
 		require.NoError(t, err)
 		gameToAdd.Tags = model.GetTagNames(tags)
 		gameToAdd.Genres = model.GetGenreNames(genres)
@@ -49,17 +27,18 @@ func TestAddGame(t *testing.T) {
 		gameToAdd.CoverImage = expectedImage
 
 		request := gamev4.AddGameRequest{Game: gameToAdd}
-		resp, err := grpcClient.AddGame(ctx, &request)
+		resp, err := suite.GrpcClient.AddGame(ctx, &request)
 		hadlerchecker.AssertAddGame(t, err, resp)
 
-		gameDB, err := db.GetGameByID(ctx, resp.GetGameId())
+		gameDB, err := suite.Db.GetGameByID(ctx, resp.GetGameId())
 		require.NoError(t, err)
-		checkers.AssertAddGameRequestAndDB(ctx, t, &request, *gameDB, s3)
+		// Не передавать s3
+		checkers.AssertAddGameRequestAndDB(ctx, t, &request, *gameDB, suite.S3)
 
 	})
 	t.Run("Тест ручки AddGame; Игра не создается если передан хотя бы один несущетвующий тэг", func(t *testing.T) {
 		gameToAdd := random.RandomAddGameRequest()
-		tags, err := db.GetTags(ctx)
+		tags, err := suite.Db.GetTags(ctx)
 		require.NoError(t, err)
 		gameToAdd.Tags = append(model.GetTagNames(tags), gameToAdd.Tags...)
 		gameToAdd.Genres = nil
@@ -68,13 +47,13 @@ func TestAddGame(t *testing.T) {
 		gameToAdd.CoverImage = expectedImage
 
 		request := gamev4.AddGameRequest{Game: gameToAdd}
-		resp, err := grpcClient.AddGame(ctx, &request)
+		resp, err := suite.GrpcClient.AddGame(ctx, &request)
 
 		hadlerchecker.AssertAddGameTagNotFound(t, err, resp)
 	})
 	t.Run("Тест ручки AddGame; Игра не создается если передан хотя бы один несущетвующий жанр", func(t *testing.T) {
 		gameToAdd := random.RandomAddGameRequest()
-		genres, err := db.GetGenres(ctx)
+		genres, err := suite.Db.GetGenres(ctx)
 		require.NoError(t, err)
 		gameToAdd.Genres = append(model.GetGenreNames(genres), gameToAdd.Genres...)
 		gameToAdd.Tags = nil
@@ -83,7 +62,7 @@ func TestAddGame(t *testing.T) {
 		gameToAdd.CoverImage = expectedImage
 
 		request := gamev4.AddGameRequest{Game: gameToAdd}
-		resp, err := grpcClient.AddGame(ctx, &request)
+		resp, err := suite.GrpcClient.AddGame(ctx, &request)
 
 		hadlerchecker.AssertAddGameGenreNotFound(t, err, resp)
 
@@ -96,7 +75,7 @@ func TestAddGame(t *testing.T) {
 		require.NoError(t, err)
 		gameToAdd.CoverImage = expectedImage
 		request := gamev4.AddGameRequest{Game: gameToAdd}
-		resp, err := grpcClient.AddGame(ctx, &request)
+		resp, err := suite.GrpcClient.AddGame(ctx, &request)
 		require.NoError(t, err)
 		require.NotZero(t, resp.GetGameId())
 
@@ -105,7 +84,7 @@ func TestAddGame(t *testing.T) {
 		duplicateGame.ReleaseDate = gameToAdd.GetReleaseDate()
 
 		duplicateRequest := gamev4.AddGameRequest{Game: duplicateGame}
-		resp, err = grpcClient.AddGame(ctx, &duplicateRequest)
+		resp, err = suite.GrpcClient.AddGame(ctx, &duplicateRequest)
 
 		hadlerchecker.AssertAddGameDuplicateGame(t, err, resp)
 	})
--- tests/game_service_get_top_games_test.go
+++ tests/game_service_get_top_games_test.go
@@ -1,48 +1,28 @@
 package tests
 
 import (
-	"context"
 	"math/rand/v2"
-	"net"
-	"strconv"
 	"testing"
 
 	"github.com/brianvoe/gofakeit/v7"
-	"github.com/sariya23/game_service/internal/config"
-	"github.com/sariya23/game_service/internal/lib/mockslog"
 	"github.com/sariya23/game_service/internal/lib/random"
 	"github.com/sariya23/game_service/internal/model"
 	"github.com/sariya23/game_service/internal/model/dto"
-	"github.com/sariya23/game_service/internal/storage/postgresql"
 	"github.com/sariya23/game_service/tests/checkers"
 	"github.com/sariya23/game_service/tests/helpers"
+	"github.com/sariya23/game_service/tests/suite"
 	gamev4 "github.com/sariya23/proto_api_games/v4/gen/game"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func TestGetTopGames(t *testing.T) {
-	ctx := context.Background()
-	cfg := config.MustLoadByPath("../config/local.env")
-	db := postgresql.MustNewConnection(ctx, mockslog.NewDiscardLogger(), cfg.Postgres.PostgresURL)
-	conn, err := grpc.NewClient(
-		net.JoinHostPort("127.0.0.1", strconv.Itoa(cfg.Server.GrpcServerPort)),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	if err != nil || conn == nil {
-		t.Fatalf("cannot start client; err = %v", err)
-	}
-	grpcClient := gamev4.NewGameServiceClient(conn)
-	if grpcClient == nil {
-		t.Fatal("cannot create grpcClient")
-	}
+	ctx, suite := suite.NewSuite(t)
 	t.Run("Тест ручки GetTopGame; При пустом запросе вовзращается 10 игр", func(t *testing.T) {
-		expctedGames, err := db.GetTopGames(ctx, dto.GameFilters{}, 10)
+		expctedGames, err := suite.Db.GetTopGames(ctx, dto.GameFilters{}, 10)
 		require.NoError(t, err)
 		req := gamev4.GetTopGamesRequest{}
-		response, err := grpcClient.GetTopGames(ctx, &req)
+		response, err := suite.GrpcClient.GetTopGames(ctx, &req)
 		require.NoError(t, err)
 
 		require.Equal(t, len(expctedGames), len(response.Games))
@@ -53,7 +33,7 @@ func TestGetTopGames(t *testing.T) {
 		}
 	})
 	t.Run("Тест ручки GetTopGame; Фильтрация по жанрам, без лимита", func(t *testing.T) {
-		genres, err := db.GetGenres(ctx)
+		genres, err := suite.Db.GetGenres(ctx)
 		require.NoError(t, err)
 		genres = random.Sample(genres, 3)
 		expectedGenreNames := model.GetGenreNames(genres)
@@ -61,14 +41,14 @@ func TestGetTopGames(t *testing.T) {
 		gameToAdd := random.RandomAddGameRequest()
 		gameToAdd.Genres = expectedGenreNames
 		gameToAdd.Tags = nil
-		_, err = grpcClient.AddGame(ctx, &gamev4.AddGameRequest{Game: gameToAdd})
+		_, err = suite.GrpcClient.AddGame(ctx, &gamev4.AddGameRequest{Game: gameToAdd})
 		require.NoError(t, err)
 
 		expectedGenreNames = append(expectedGenreNames, gofakeit.MovieGenre())
-		expctedGames, err := db.GetTopGames(ctx, dto.GameFilters{Genres: expectedGenreNames}, 10)
+		expctedGames, err := suite.Db.GetTopGames(ctx, dto.GameFilters{Genres: expectedGenreNames}, 10)
 		require.NoError(t, err)
 		req := gamev4.GetTopGamesRequest{Genres: expectedGenreNames}
-		response, err := grpcClient.GetTopGames(ctx, &req)
+		response, err := suite.GrpcClient.GetTopGames(ctx, &req)
 		require.NoError(t, err)
 
 		require.Equal(t, len(expctedGames), len(response.Games))
@@ -76,7 +56,7 @@ func TestGetTopGames(t *testing.T) {
 			gameDB := expctedGames[i]
 			gameSRV := response.Games[i]
 
-			fullGame, err := db.GetGameByID(ctx, gameDB.GameID)
+			fullGame, err := suite.Db.GetGameByID(ctx, gameDB.GameID)
 			require.NoError(t, err)
 			fullGameGenreNames := model.GetGenreNames(fullGame.Genres)
 			assert.True(t, helpers.HasIntersection(expectedGenreNames, fullGameGenreNames))
@@ -84,7 +64,7 @@ func TestGetTopGames(t *testing.T) {
 		}
 	})
 	t.Run("Тест ручки GetTopGame; Фильтрация по тэгам, без лимита", func(t *testing.T) {
-		tags, err := db.GetTags(ctx)
+		tags, err := suite.Db.GetTags(ctx)
 		require.NoError(t, err)
 		tags = random.Sample(tags, 3)
 		expectedTagNames := model.GetTagNames(tags)
@@ -92,15 +72,15 @@ func TestGetTopGames(t *testing.T) {
 		gameToAdd := random.RandomAddGameRequest()
 		gameToAdd.Tags = expectedTagNames
 		gameToAdd.Genres = nil
-		_, err = grpcClient.AddGame(ctx, &gamev4.AddGameRequest{Game: gameToAdd})
+		_, err = suite.GrpcClient.AddGame(ctx, &gamev4.AddGameRequest{Game: gameToAdd})
 		require.NoError(t, err)
 
 		expectedTagNames = append(expectedTagNames, gofakeit.BookGenre())
-		expctedGames, err := db.GetTopGames(ctx, dto.GameFilters{Tags: expectedTagNames}, 10)
+		expctedGames, err := suite.Db.GetTopGames(ctx, dto.GameFilters{Tags: expectedTagNames}, 10)
 		require.NoError(t, err)
 		expectedTagNames = append(expectedTagNames, gofakeit.BookGenre())
 		req := gamev4.GetTopGamesRequest{Tags: expectedTagNames}
-		response, err := grpcClient.GetTopGames(ctx, &req)
+		response, err := suite.GrpcClient.GetTopGames(ctx, &req)
 		require.NoError(t, err)
 
 		require.Equal(t, len(expctedGames), len(response.Games))
@@ -108,7 +88,7 @@ func TestGetTopGames(t *testing.T) {
 			gameDB := expctedGames[i]
 			gameSRV := response.Games[i]
 
-			fullGame, err := db.GetGameByID(ctx, gameDB.GameID)
+			fullGame, err := suite.Db.GetGameByID(ctx, gameDB.GameID)
 			require.NoError(t, err)
 			fullGameTagsNames := model.GetTagNames(fullGame.Tags)
 
@@ -117,9 +97,9 @@ func TestGetTopGames(t *testing.T) {
 		}
 	})
 	t.Run("Тест ручки GetTopGame; Фильтрация по тэгам и жанрам", func(t *testing.T) {
-		tags, err := db.GetTags(ctx)
+		tags, err := suite.Db.GetTags(ctx)
 		require.NoError(t, err)
-		genres, err := db.GetGenres(ctx)
+		genres, err := suite.Db.GetGenres(ctx)
 		require.NoError(t, err)
 		gameTagNames := model.GetTagNames(random.Sample(tags, 2))
 		gameGenreNames := model.GetGenreNames(random.Sample(genres, 2))
@@ -127,10 +107,10 @@ func TestGetTopGames(t *testing.T) {
 		gameToAdd.Genres = gameGenreNames
 		gameToAdd.Tags = gameTagNames
 
-		_, err = grpcClient.AddGame(ctx, &gamev4.AddGameRequest{Game: gameToAdd})
+		_, err = suite.GrpcClient.AddGame(ctx, &gamev4.AddGameRequest{Game: gameToAdd})
 		require.NoError(t, err)
 
-		expectedGames, err := db.GetTopGames(ctx, dto.GameFilters{
+		expectedGames, err := suite.Db.GetTopGames(ctx, dto.GameFilters{
 			Genres: gameGenreNames,
 			Tags:   gameTagNames,
 		}, 10)
@@ -138,7 +118,7 @@ func TestGetTopGames(t *testing.T) {
 
 		gameGenreNames = append(gameGenreNames, gofakeit.BookGenre())
 		gameTagNames = append(gameTagNames, gofakeit.BookGenre())
-		resp, err := grpcClient.GetTopGames(ctx,
+		resp, err := suite.GrpcClient.GetTopGames(ctx,
 			&gamev4.GetTopGamesRequest{
 				Genres: gameGenreNames,
 				Tags:   gameTagNames,
@@ -151,7 +131,7 @@ func TestGetTopGames(t *testing.T) {
 			gameSRV := resp.Games[i]
 
 			assert.Equal(t, gameDB.GameID, gameSRV.ID)
-			fullGame, err := grpcClient.GetGame(ctx, &gamev4.GetGameRequest{GameId: gameSRV.ID})
+			fullGame, err := suite.GrpcClient.GetGame(ctx, &gamev4.GetGameRequest{GameId: gameSRV.ID})
 			require.NoError(t, err)
 			assert.True(t, helpers.HasIntersection(fullGame.Game.Genres, gameGenreNames))
 			assert.True(t, helpers.HasIntersection(fullGame.Game.Tags, gameTagNames))
@@ -159,9 +139,9 @@ func TestGetTopGames(t *testing.T) {
 		}
 	})
 	t.Run("Тест ручки GetTopGame; Фильтрация по тэгам, жанрам и году", func(t *testing.T) {
-		tags, err := db.GetTags(ctx)
+		tags, err := suite.Db.GetTags(ctx)
 		require.NoError(t, err)
-		genres, err := db.GetGenres(ctx)
+		genres, err := suite.Db.GetGenres(ctx)
 		require.NoError(t, err)
 		gameTagNames := model.GetTagNames(random.Sample(tags, 2))
 		gameGenreNames := model.GetGenreNames(random.Sample(genres, 2))
@@ -169,19 +149,19 @@ func TestGetTopGames(t *testing.T) {
 		gameToAdd.Genres = gameGenreNames
 		gameToAdd.Tags = gameTagNames
 
-		_, err = grpcClient.AddGame(ctx, &gamev4.AddGameRequest{Game: gameToAdd})
+		_, err = suite.GrpcClient.AddGame(ctx, &gamev4.AddGameRequest{Game: gameToAdd})
 		require.NoError(t, err)
 
 		gameGenreNames = append(gameGenreNames, gofakeit.BookGenre())
 		gameTagNames = append(gameTagNames, gofakeit.BookGenre())
-		expectedGames, err := db.GetTopGames(ctx, dto.GameFilters{
+		expectedGames, err := suite.Db.GetTopGames(ctx, dto.GameFilters{
 			Genres:      gameGenreNames,
 			Tags:        gameTagNames,
 			ReleaseYear: gameToAdd.ReleaseDate.Year,
 		}, 10)
 		require.NoError(t, err)
 
-		resp, err := grpcClient.GetTopGames(ctx,
+		resp, err := suite.GrpcClient.GetTopGames(ctx,
 			&gamev4.GetTopGamesRequest{
 				Genres: gameGenreNames,
 				Tags:   gameTagNames,
@@ -195,7 +175,7 @@ func TestGetTopGames(t *testing.T) {
 			gameSRV := resp.Games[i]
 
 			assert.Equal(t, gameDB.GameID, gameSRV.ID)
-			fullGame, err := grpcClient.GetGame(ctx, &gamev4.GetGameRequest{GameId: gameSRV.ID})
+			fullGame, err := suite.GrpcClient.GetGame(ctx, &gamev4.GetGameRequest{GameId: gameSRV.ID})
 			require.NoError(t, err)
 			assert.True(t, helpers.HasIntersection(fullGame.Game.Genres, gameGenreNames))
 			assert.True(t, helpers.HasIntersection(fullGame.Game.Tags, gameTagNames))
@@ -204,10 +184,10 @@ func TestGetTopGames(t *testing.T) {
 	})
 	t.Run("Тест ручки GetTopGame; Указан только лимит", func(t *testing.T) {
 		limit := rand.IntN(15) + 1
-		expctedGames, err := db.GetTopGames(ctx, dto.GameFilters{}, uint32(limit))
+		expctedGames, err := suite.Db.GetTopGames(ctx, dto.GameFilters{}, uint32(limit))
 		require.NoError(t, err)
 		req := gamev4.GetTopGamesRequest{Limit: uint32(limit)}
-		response, err := grpcClient.GetTopGames(ctx, &req)
+		response, err := suite.GrpcClient.GetTopGames(ctx, &req)
 		require.NoError(t, err)
 
 		require.Equal(t, len(expctedGames), len(response.Games))
