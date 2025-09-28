6dfaa0ea9ef808758ac7fbd56f620da6af002c9c
nikita <dayzwaitik123@yandex.ru>
2025-09-28 23:39:33 +0300

    добавил доку к методу

--- internal/service/game/update_game_status.go
+++ internal/service/game/update_game_status.go
@@ -10,6 +10,11 @@ import (
 	gamev4 "github.com/sariya23/proto_api_games/v4/gen/game"
 )
 
+// UpdateGameStatus меняет статус видимости игры.
+// Нельзя:
+// DRAFT -> PUBLISH
+// PUBLISH -> PENDING
+// PUBLISH -> DRAFT
 func (gameService *GameService) UpdateGameStatus(ctx context.Context, gameID uint64, newStatus gamev4.GameStatusType) error {
 	const operationPlace = "gameservice.UpdateGameStatus"
 	log := gameService.log.With("operationPlace", operationPlace)
