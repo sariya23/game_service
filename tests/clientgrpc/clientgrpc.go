93fd35c15cc0b505aff33f3ec6f548573037aaf3
nikita <dayzwaitik123@yandex.ru>
2025-10-06 22:37:54 +0300

    Начал писать тесты на хендлеры

---
+++ tests/clientgrpc/clientgrpc.go
@@ -0,0 +1,40 @@
+package clientgrpc
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/sariya23/game_service/internal/config"
+	"github.com/sariya23/proto_api_games/v5/gen/gamev2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type GameServiceTestClient struct {
+	cl gamev2.GameServiceClient
+}
+
+func NewGameServiceTestClient() *GameServiceTestClient {
+	cfg := config.MustLoadByPath(filepath.Join("..", "..", "..", "..", "config", "test.env"))
+	conn, err := grpc.NewClient(
+		fmt.Sprintf("%s:%s", cfg.Server.GRPCServerHost, cfg.Server.GrpcServerPort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		panic(err)
+	}
+	grpcClient := gamev2.NewGameServiceClient(conn)
+	if grpcClient == nil {
+		panic(err)
+	}
+	return &GameServiceTestClient{grpcClient}
+}
+
+func (g *GameServiceTestClient) SetUp(t *testing.T) {
+	t.Helper()
+}
+
+func (g *GameServiceTestClient) TearDown(t *testing.T) {
+	t.Helper()
+}
---
+++ tests/intergration/handler_test/get_game_test.go
@@ -0,0 +1,11 @@
+//go:build integrations
+
+package handler
+
+import "testing"
+
+func TestGetGame(t *testing.T) {
+	t.Run("Успешное получение игры", func(t *testing.T) {
+
+	})
+}
